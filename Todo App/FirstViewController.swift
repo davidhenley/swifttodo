2f08ecd0be33b0990e5019977000186b7a779a14
David Henley <henleydr@gmail.com>
2017-06-27 09:50:23 -0500

    Finished

--- Todo App/FirstViewController.swift
+++ Todo App/FirstViewController.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    var todos = [String]()
+    let defaults = UserDefaults.standard
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
 
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = todos[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        // If swipe left on item
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            // Remove this item from array
+            todos.remove(at: indexPath.row)
+            
+            // Reload table data
+            table.reloadData()
+            
+            // Save the todos back to storage
+            defaults.set(todos, forKey: "todos")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        todos = defaults.object(forKey: "todos") as? [String] ?? [String]()
+        
+        table.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
--- Todo App/SecondViewController.swift
+++ Todo App/SecondViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var todoTextField: UITextField!
 
+    @IBAction func addTodo(_ sender: Any) {
+        let todo = todoTextField.text!
+        let defaults = UserDefaults.standard
+        
+        // If empty don't do anything
+        if todo.characters.count == 0 { return }
+        
+        // Check for existing todos in storage
+        var todos = defaults.object(forKey: "todos") as? [String] ?? [String]()
+        
+        // Add todo to array
+        todos.append(todo)
+        
+        // Overwrite the todos in storage
+        defaults.set(todos, forKey: "todos")
+        
+        // Set the textfield back to blank
+        todoTextField.text = ""
+        
+        // TODO: Remove on production
+        print("Todos: \(todos)")
+        
+        // TODO: Navigate back to todo list
+        // TODO: Save on return
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +47,15 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        addTodo(textField)
+        return true
+    }
 
 }
 
